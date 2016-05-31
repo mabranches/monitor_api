462138ccf59c3620c403f74c5d48059d397b03bb
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-31 20:59:05 -0300

    methods to create a new item and delete table for models

--- app/models/concerns/model.rb
+++ app/models/concerns/model.rb
@@ -5,4 +5,10 @@ module Model
   def count
     $dynamodb.scan(table_name: @@table_name).count
   end
+  def create(item)
+    $dynamodb.put_item(table_name:@@table_name, item: usage)
+  end
+  def delete_table
+    $dynamodb.delete_table(table_name:@@table_name) rescue nil
+  end
 end
