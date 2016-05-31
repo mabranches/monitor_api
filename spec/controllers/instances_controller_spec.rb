fe2a843ae0f51715e259839671dbaf88f384c03f
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-31 09:06:30 -0300

    test cleanup and post of new usage and process test

--- spec/controllers/instances_controller_spec.rb
+++ spec/controllers/instances_controller_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe InstancesController, type: :controller do
 
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {
+       "id" => 'test_id',
+      "instance_id"=>"test_id",
+      "disk"=>"51",
+      "mem"=>"9",
+      "cpu"=>"1",
+      "process"=>{"user"=>"root", "pid"=>"1", "cpu"=>"0.0", "mem"=>"0.0", "command"=>"/sbin/init"}
+    }
   }
 
   let(:invalid_attributes) {
@@ -13,43 +20,49 @@ RSpec.describe InstancesController, type: :controller do
   let(:valid_session) { {} }
 
   describe "GET #usage" do
-    it "assigns all instances as @instances" do
-      instance = Instance.create! valid_attributes
-      get :usage, {}, valid_session
-      expect(assigns(:instances)).to eq([instance])
+    it "should return all usage from last 24h" do
+     # instance = Instance.create! valid_attributes
+     # get :usage, {}, valid_session
+     # expect(assigns(:instances)).to eq([instance])
+    end
+  end
+
+  describe "get #processes" do
+    it "should return all processes" do
+    end
+  end
+  describe "get #status" do
+    it "should return the machine status" do
+    end
+  end
+
+  describe "get #report" do
+    it "should return a report" do
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new Instance" do
+      it "creates a new usage" do
         expect {
-          post :create, {:instance => valid_attributes}, valid_session
-        }.to change(Instance, :count).by(1)
-      end
-
-      it "assigns a newly created instance as @instance" do
-        post :create, {:instance => valid_attributes}, valid_session
-        expect(assigns(:instance)).to be_a(Instance)
-        expect(assigns(:instance)).to be_persisted
-      end
-
-      it "redirects to the created instance" do
-        post :create, {:instance => valid_attributes}, valid_session
-        expect(response).to redirect_to(Instance.last)
+          post :create, {:id => "test_id", :instance => valid_attributes}, valid_session
+          #post :create,  valid_attributes, valid_session
+        }.to change(Usage, :count).by(1)
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved instance as @instance" do
-        post :create, {:instance => invalid_attributes}, valid_session
-        expect(assigns(:instance)).to be_a_new(Instance)
+        #post :create, {:instance => invalid_attributes}, valid_session
+        #expect(assigns(:instance)).to be_a_new(Instance)
       end
 
-      it "re-renders the 'new' template" do
-        post :create, {:instance => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
     end
+  end
+
+  describe "POST #stop" do
+  end
+
+  describe "POST #start" do
   end
 end
