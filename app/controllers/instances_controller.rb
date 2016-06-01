0d314f201b19d4550183692d6ce60ebe9c3fbcfd
Manoel Rebelo Abranches <abranches@gmail.com>
2016-06-01 06:50:19 -0300

    Use models on controllers

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,67 +1,46 @@
 class InstancesController < ApplicationController
   before_action :set_instance, only: [:show, :update, :destroy]
 
-  # GET /instances
-  # GET /instances.json
   def usage
-    items = $dynamodb.scan(
-      table_name:'instances',
-      filter_expression: "usage_time < :now and usage_time > :24h_before",
-      expression_attribute_values:{
-        ":now": Time.now.utc.iso8601,
-        ":24h_before":(Time.now - 24.hour).utc.iso8601
-      }).data.items
 
+   items = Usage.get(Time.now.utc.iso8601, (Time.now - 24.hour).utc.iso8601)
     result = {}
+
     items.each do |item|
-      #initialize
-      result[item["instance_id"]] ||= {}
-      result[item["instance_id"]]["mem"] ||= []
-      result[item["instance_id"]]["disk"] ||= []
-      result[item["instance_id"]]["cpu"] ||= []
-      result[item["instance_id"]]["usage_time"] ||= []
-      #add_item
-      result[item["instance_id"]]["mem"] << item["mem"]
-      result[item["instance_id"]]["disk"] << item["disk"]
-      result[item["instance_id"]]["cpu"] << item["cpu"]
-      result[item["instance_id"]]["usage_time"] << item["usage_time"]
+      add_item_hash(result, item)
     end
 
     render json: result
   end
 
-  # POST /instances
-  # POST /instances.json
   def create
     #create connection at a initializer
     time = Time.now.utc.iso8601
+    instance = params[:instance]
     usage = {
-      instance_id: params[:instance_id],
-      cpu: params[:cpu],
-      mem: params[:mem],
-      disk: params[:disk],
+      instance_id: params[:id],
+      cpu: instance[:cpu],
+      mem: instance[:mem],
+      disk: instance[:disk],
       usage_time: time
     }
 
     process = {
-      instance_id: params[:instance_id],
-      process: (params[:process].values rescue [])
+      instance_id: instance[:instance_id],
+      process: (instance[:process].values rescue [])
     }
 
-    $dynamodb.put_item(table_name:'instances', item: usage)
-    $dynamodb.put_item(table_name:'processes', item: process)
+    Usage.create(usage)
+    Process.create(process)
 
     render json: [usage, process], status: :created
     rescue Exception => e
-      puts e.message
+      logger.error(e.message)
       render json: {error:e.message}, status: :unprocessable_entity
   end
 
   def processes
-    items = $dynamodb.scan(
-      table_name:'processes'
-      ).data.items
-
+    items = Process.get
     process_items = {}
     items.each do |item|
       process_items[item["instance_id"]] = item
@@ -102,5 +81,15 @@ class InstancesController < ApplicationController
     ec2 = Aws::EC2::Resource.new(region: 'us-west-2')
     status = ec2.instance(instance_id).state.name rescue "unknown"
     render json: {instance_id: instance_id, status: status}
+  end
+
+  private
+
+  def add_item_hash(result, item)
+      result[item["instance_id"]] ||= Hash.new { |h, k| h[k] = [] }
+      result[item["instance_id"]]["mem"] << item["mem"]
+      result[item["instance_id"]]["disk"] << item["disk"]
+      result[item["instance_id"]]["cpu"] << item["cpu"]
+      result[item["instance_id"]]["usage_time"] << item["usage_time"]
   end
 end
