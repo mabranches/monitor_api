e2e50dae586dfe818bb30c452498593961fd5f5a
Manoel Rebelo Abranches <abranches@gmail.com>
2016-06-01 04:56:28 -0300

    test available routes

--- spec/routing/instances_routing_spec.rb
+++ spec/routing/instances_routing_spec.rb
@@ -3,36 +3,28 @@ require "rails_helper"
 RSpec.describe InstancesController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/instances").to route_to("instances#index")
+    it "routes to #usage" do
+      expect(:get => "/instances/usage").to route_to("instances#usage")
     end
 
-    it "routes to #new" do
-      expect(:get => "/instances/new").to route_to("instances#new")
+    it "routes to #processes" do
+      expect(:get => "/instances/processes").to route_to("instances#processes")
     end
 
-    it "routes to #show" do
-      expect(:get => "/instances/1").to route_to("instances#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/instances/1/edit").to route_to("instances#edit", :id => "1")
+    it "routes to #status" do
+      expect(:get => "/instances/instance_id/status").to route_to("instances#status", :id => "instance_id")
     end
 
     it "routes to #create" do
-      expect(:post => "/instances").to route_to("instances#create")
+      expect(:post => "/instances/instance_id").to route_to("instances#create", :id => "instance_id")
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/instances/1").to route_to("instances#update", :id => "1")
+    it "routes to #start" do
+      expect(:post => "/instances/instance_id/start").to route_to("instances#start", :id => "instance_id")
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/instances/1").to route_to("instances#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/instances/1").to route_to("instances#destroy", :id => "1")
+    it "routes to #stop" do
+      expect(:post => "/instances/instance_id/stop").to route_to("instances#stop", :id => "instance_id")
     end
 
   end
