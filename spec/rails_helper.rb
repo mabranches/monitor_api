073f9f9b7a684e0770e54e817d6613ab219280d5
Manoel Rebelo Abranches <abranches@gmail.com>
2016-06-06 08:03:32 -0300

    refactory aux method to use models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,16 +8,16 @@ require 'rspec/rails'
 
 class DynSpecData
   def self.create_usage_data(usage_data=[])
-    create_dynamo_db_data('instances', usage_data)
+    create_dynamo_db_data(Usage, usage_data)
   end
 
   def self.create_process_data(process_data=[])
-    create_dynamo_db_data('processes', process_data)
+    create_dynamo_db_data(ProcessList, process_data)
   end
 private
-  def self.create_dynamo_db_data(table, data)
+  def self.create_dynamo_db_data(klass, data)
     data.each do |datum|
-     $dynamodb.put_item(table_name:table, item: datum)
+      klass.create(datum)
     end
   end
 end
